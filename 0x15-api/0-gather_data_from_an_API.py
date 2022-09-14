b2fa19e7f885437c5f7aafe79c636bbf87590cf3
fernan17893 <fernan17893@gmail.com>
2022-09-13 22:31:16 -0400

    4get the checkers

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns API information"""
+""" Returns API information """
 import json
 import requests
 import sys
@@ -8,27 +8,21 @@ if __name__ == "__main__":
     if sys.argv[1].isdigit() is True:
         user_id = int(sys.argv[1])
 
-"""Fetch users from API"""
-user_api = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
-users = json.loads(user_api.text)
+    user_api = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    users = json.loads(user_api.text)
+    todo_api = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = json.loads(todo_api.text)
+    total_tasks = 0
+    tasks_done = 0
 
+    for task in todos:
+        total_tasks += 1
+        if task['completed']:
+            tasks_done += 1
+    print('Employee {} is done with tasks ({}/{}):'
+          .format(users['name'], tasks_done, total_tasks))
 
-"""Fetch todos list"""
-todo_api = requests.get('https://jsonplaceholder.typicode.com/todos')
-todos = json.loads(todo_api.text)
-
-
-"""Iterate through todos list for task done/total tasks"""
-total_tasks = 0
-tasks_done = 0
-for task in todos:
-    total_tasks += 1
-    if task['completed']:
-        tasks_done += 1
-print('Employee {} is done with tasks ({}/{}):'
-      .format(users['name'], tasks_done, total_tasks))
-
-for task in todos:
-    if task['completed']:
-        print('\t {}'.format(task['title']))
+    for task in todos:
+        if task['completed']:
+            print('\t {}'.format(task['title']))
