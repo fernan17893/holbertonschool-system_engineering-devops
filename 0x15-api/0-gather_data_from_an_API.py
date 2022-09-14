87d076251a2b767c82f369e506bea9190babe5e1
fernan17893 <fernan17893@gmail.com>
2022-09-14 10:45:17 -0400

    finally correct output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Returns API information """
+""" 
+Returns API information
+"""
+
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
+    """ Check first arg as user ID to return information """
     if sys.argv[1].isdigit() is True:
         user_id = int(sys.argv[1])
 
